35b51cbe744480a151297380e056f2e697d617af
GwenRaq <gwen.raq@gmail.com>
2016-12-14 09:18:09 -1000

    3

---
+++ js/app.js
@@ -0,0 +1,17 @@
+console.log("Sanity check");
+var box = document.getElementById('box');
+function okay() {
+	box.style.backgroundColor = "blue";
+}
+
+function leave(){
+	box.style.backgroundColor = "red";
+}
+
+function another(){
+	box.style.backgroundColor = "lightblue";
+}
+
+function out(){
+	box.style.backgroundColor = "purple";
+}
